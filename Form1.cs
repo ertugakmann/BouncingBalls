20d2f28b763739ef0033923dd1656adfc68d8961
235303 <235303@live.brock.ac.uk>
2025-06-19 09:48:35 +0100

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BouncingBalls
+{
+
+    public partial class Form1 : Form
+    {
+        Mover mover;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.DoubleBuffered = true;
+            this.WindowState = FormWindowState.Maximized;
+
+            Timer timer = new Timer();
+            timer.Enabled = true;
+            timer.Interval = 10;
+            timer.Tick += TimerTick;
+            this.Paint += Form1_Paint;
+            mover = new Mover(this.Width, this.Height);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            mover.Update();
+            mover.Display(e.Graphics);
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+           Invalidate();
+        }
+    }
+}
---
+++ Mover.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.Numerics;
+
+namespace BouncingBalls
+{
+    class Mover
+    {
+        static Random rnd = new Random();
+        Vector2 location;
+        Vector2 velocity;
+
+        public Mover(int x, int y)
+        {
+            location = new Vector2(rnd.Next(x), rnd.Next(y));
+            velocity = new Vector2(rnd.Next(-10, 11), rnd.Next(-10, 11));
+        }
+
+        public void Update()
+        {
+            velocity.X = rnd.Next(-10, 11);
+            velocity.Y = rnd.Next(-10, 11);
+
+            location = Vector2.Add(location, velocity);
+        }
+
+        public void Display(Graphics e)
+        {
+            e.FillEllipse(Brushes.Pink, location.X, location.Y, 100, 100);
+        }
+    }
+}
