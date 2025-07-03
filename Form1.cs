4a65e4c3c0bda17c633d318d828985e1ad49de5b
235303 <235303@live.brock.ac.uk>
2025-07-03 09:21:02 +0100

    Moving Balls Created

--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -10,38 +11,49 @@ using System.Windows.Forms;
 
 namespace BouncingBalls
 {
-
     public partial class Form1 : Form
     {
         Mover mover;
+        List<Mover> movers = new List<Mover>();
 
         public Form1()
         {
             InitializeComponent();
         }
 
+
+        Random random = new Random();
         private void Form1_Load(object sender, EventArgs e)
         {
             this.DoubleBuffered = true;
             this.WindowState = FormWindowState.Maximized;
-
             Timer timer = new Timer();
             timer.Enabled = true;
-            timer.Interval = 10;
-            timer.Tick += TimerTick;
+            timer.Interval = 5;
+            timer.Tick += Timer_Tick;
             this.Paint += Form1_Paint;
-            mover = new Mover(this.Width, this.Height);
+            for (int i = 0; i < 1000; i++)
+            {
+                Color randomColor = Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+                mover = new Mover(this.Width, this.Height, this, randomColor);
+                movers.Add(mover);
+            }
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            mover.Update();
-            mover.Display(e.Graphics);
+            //mover.Update();
+            //mover.Display(e.Graphics);
+            foreach (var mover in movers)
+            {
+                mover.Update();
+                mover.Display(e.Graphics);
+            }
         }
 
-        private void TimerTick(object sender, EventArgs e)
+        private void Timer_Tick(object sender, EventArgs e)
         {
-           Invalidate();
+            Invalidate();
         }
     }
 }
--- Mover.cs
+++ Mover.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Drawing;
 using System.Numerics;
-
+using System.Windows.Forms;
+ 
 namespace BouncingBalls
 {
     class Mover
@@ -9,24 +10,69 @@ namespace BouncingBalls
         static Random rnd = new Random();
         Vector2 location;
         Vector2 velocity;
-
-        public Mover(int x, int y)
+        int formWidth, formHeight;
+        Vector2 accerlation;
+        Vector2 mouse = new Vector2();
+        float topSpeed;
+        Form frm;
+        Color color;
+ 
+ 
+        public Mover(int width, int height, Form theForm, Color color)
         {
-            location = new Vector2(rnd.Next(x), rnd.Next(y));
-            velocity = new Vector2(rnd.Next(-10, 11), rnd.Next(-10, 11));
+            formWidth = width;
+            formHeight = height;
+            location = new Vector2(rnd.Next(width), rnd.Next(height));
+            velocity = new Vector2(0, 0);
+            accerlation = new Vector2((float)-0.001, (float)0.001);
+            frm = theForm;
+            topSpeed = 10;
+            this.color = color;
         }
-
+ 
+ 
+        float Mag(Vector2 theVector)
+        {
+            return (float)Math.Sqrt(theVector.X * theVector.X + theVector.Y * theVector.Y);
+        }
+ 
+ 
+        public Vector2 Limit(float theLimit, Vector2 theVector)
+        {
+            if (Mag(theVector) > theLimit)
+            {
+                // this is off stack overflow the 10 is the limit 
+                theVector.X = theVector.X * theLimit / Mag(theVector);
+                theVector.Y = theVector.Y * theLimit / Mag(theVector);
+            }
+            return theVector;
+        }
+ 
         public void Update()
         {
-            velocity.X = rnd.Next(-10, 11);
-            velocity.Y = rnd.Next(-10, 11);
-
-            location = Vector2.Add(location, velocity);
+            Point cp = frm.PointToClient(Cursor.Position);
+            mouse.X = cp.X;
+            mouse.Y = cp.Y;
+            Vector2 dir = Vector2.Subtract(mouse, location);
+            dir = Vector2.Normalize(dir);
+            dir = Vector2.Multiply(0.6f, dir);
+            accerlation = dir;
+            velocity = Vector2.Add(velocity, accerlation);
+            velocity = Limit(topSpeed, velocity);
+            location = Vector2.Add(location, velocity); 
         }
-
+ 
         public void Display(Graphics e)
         {
-            e.FillEllipse(Brushes.Pink, location.X, location.Y, 100, 100);
+ 
+            using (Brush brush = new SolidBrush(color))
+            {
+                e.FillEllipse(brush, location.X, location.Y, 20, 20);
+            }
+            //e.FillEllipse(Brushes.BlueViolet, location.X, location.Y, 20, 20);
         }
+ 
     }
+ 
+ 
 }
